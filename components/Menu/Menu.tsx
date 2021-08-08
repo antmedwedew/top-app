c57e4f45c5a3fbf1b385b3c899f2027421cd577e
Anton Medwedew <medwedew.anton2017@yandex.ru>
2021-08-08 14:49:22 +0300

    render menu

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,13 +1,79 @@
 import { useContext } from 'react';
 import { AppContext } from '../../context/app.context';
+import { firstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
+import { TopLevelCategory } from '../../interfaces/topPage.interface';
+
 import styles from './Menu.module.css';
+import classNames from "classnames";
+
+import { CapIcon } from '../../public/icons/CapIcon';
+import { CloudIcon } from '../../public/icons/CloudIcon';
+import { BookIcon } from '../../public/icons/BookIcon';
+import { BoxIcon } from '../../public/icons/BoxIcon';
+
+
+const firstLevelMenu: firstLevelMenuItem[] = [
+  { route: 'courses', name: 'Курсы', icon: <CapIcon />, id: TopLevelCategory.Courses },
+  { route: 'services', name: 'Сервисы', icon: <CloudIcon />, id: TopLevelCategory.Services },
+  { route: 'books', name: 'Книги', icon: <BookIcon />, id: TopLevelCategory.Books },
+  { route: 'products', name: 'Товары', icon: <BoxIcon />, id: TopLevelCategory.Products },
+];
 
 export const Menu = (): JSX.Element => {
   const { menu, setMenu, firstCategory } = useContext(AppContext);
 
+  const buildFirstLevel = () => {
+    return (
+      <>
+        {firstLevelMenu.map(m => (
+          <div key={m.id}>
+            <a href={`/${m.route}`}>
+              <div className={classNames(styles.firstLevel, {
+                [styles.firstLevelActive]: m.id == firstCategory
+              })}>
+                {m.icon}
+                <span>{m.name}</span>
+              </div>
+            </a>
+            {m.id == firstCategory && buildSecondLevel(m)}
+          </div>
+        ))}
+      </>
+    );
+  };
+
+  const buildSecondLevel = (menuItem: firstLevelMenuItem) => {
+    return (
+      <div className={styles.secondBlock}>
+        {menu.map(item => (
+          <div key={item._id.secondCategory}>
+            <div className={styles.secondLevel}>{item._id.secondCategory}</div>
+            <div className={classNames(styles.secondLeveelBlock, {
+              [styles.secondLeveelBlockOpened]: item.isOpened
+            })}>
+              {buildThirdLevel(item.pages, menuItem.route)}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const buildThirdLevel = (pages: PageItem[], route: string) => {
+    return (
+      pages.map(p => (
+        <a href={`/${route}/${p.alias}`} key={p._id} className={classNames(styles.thirdLevel, {
+          [styles.thirdLevelActive]: false
+        })}>
+          {p.category}
+        </a>
+      ))
+    );
+  };
+
   return (
-    <ul className={styles.menu}>
-      {menu.map(item => (<li key={item._id.secondCategory}>{item._id.secondCategory}</li>))}
-    </ul>
+    <div className={styles.menu}>
+      {buildFirstLevel()}
+    </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { GetStaticProps } from "next";
-import React, { useState } from "react";
-import { Htag, Button, P, Tag, Rating } from "../components";
+import React from "react";
 import { withLayout } from "../layout/Layout";
 import axios from 'axios';
 import { MenuItem } from "../interfaces/menu.interface";
@@ -11,21 +10,10 @@ interface HomeProps extends Record<string, unknown> {
 }
 
 function Home({ menu }: HomeProps): JSX.Element {
-  const [rating, setRating] = useState<number>(1);
 
   return (
     <>
-      <Htag tag="h1">Заголовок 1 уровня</Htag>
-      <Button appearance="ghost" arrow="right">кнопка</Button>
-      <Button appearance="primary" arrow="down">кнопка</Button>
-      <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, tempore?</P>
-      <Tag size='s'>Мал</Tag>
-      <Tag color="red" href="https://github.com/">red</Tag>
-      <Tag color="green">green</Tag>
-      <Tag color="ghost">ghost</Tag>
-      <Tag color="grey">grey</Tag>
-      <Tag color="primary" >primary</Tag>
-      <Rating rating={rating} isEditable={true} setRating={setRating} />
+
     </>
   );
 }
