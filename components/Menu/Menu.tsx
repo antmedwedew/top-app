1077c58796766fe784d6825aa51168c8c4b0d662
Anton Medwedew <medwedew.anton2017@yandex.ru>
2021-09-30 20:04:43 +0300

    add component Link in menu

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,4 +1,5 @@
-import { useContext } from 'react';
+import React, { useContext } from 'react';
+import Link from 'next/link';
 import { AppContext } from '../../context/app.context';
 import { firstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
 import { TopLevelCategory } from '../../interfaces/topPage.interface';
@@ -27,14 +28,16 @@ export const Menu = (): JSX.Element => {
       <>
         {firstLevelMenu.map(m => (
           <div key={m.id}>
-            <a href={`/${m.route}`}>
-              <div className={classNames(styles.firstLevel, {
-                [styles.firstLevelActive]: m.id == firstCategory
-              })}>
-                {m.icon}
-                <span>{m.name}</span>
-              </div>
-            </a>
+            <Link href={`/${m.route}`}>
+              <a>
+                <div className={classNames(styles.firstLevel, {
+                  [styles.firstLevelActive]: m.id == firstCategory
+                })}>
+                  {m.icon}
+                  <span>{m.name}</span>
+                </div>
+              </a>
+            </Link>
             {m.id == firstCategory && buildSecondLevel(m)}
           </div>
         ))}
@@ -62,11 +65,13 @@ export const Menu = (): JSX.Element => {
   const buildThirdLevel = (pages: PageItem[], route: string) => {
     return (
       pages.map(p => (
-        <a href={`/${route}/${p.alias}`} key={p._id} className={classNames(styles.thirdLevel, {
-          [styles.thirdLevelActive]: false
-        })}>
-          {p.category}
-        </a>
+        <Link href={`/${route}/${p.alias}`} key={p._id}>
+          <a className={classNames(styles.thirdLevel, {
+            [styles.thirdLevelActive]: false
+          })}>
+            {p.category}
+          </a>
+        </Link>
       ))
     );
   };
