9c52e0aad5e166b6a163cd466860f0765fedae19
Anton Medwedew <medwedew.anton2017@yandex.ru>
2021-11-21 22:52:27 +0300

    add helper function convert price

---
+++ helpers/helpers.tsx
@@ -0,0 +1,2 @@
+// convert price
+export const priceRu = (price: number): string => price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ').concat(' ₽');
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -5,6 +5,7 @@ import { TopLevelCategory, TopPageModel } from "../../interfaces/topPage.interfa
 
 import styles from "./TopPageComponent.module.css";
 import { RoundStarIcon } from '../../public/icons/RoundStarIcon';
+import { priceRu } from "../../helpers/helpers";
 
 interface TopPageComponentProps {
   firstCategory: TopLevelCategory;
@@ -40,7 +41,7 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
           <Card className={styles.salary}>
             <div className={styles.salaryBlock}>
               <div className={styles.salaryTitle}>Начальный</div>
-              <div className={styles.salaryValue}>{page.hh?.juniorSalary}</div>
+              <div className={styles.salaryValue}>{page.hh?.juniorSalary && priceRu(page.hh.juniorSalary)}</div>
               <div className={styles.salaryRating}>
                 <RoundStarIcon color="#FC836D" />
                 <RoundStarIcon />
@@ -50,7 +51,7 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 
             <div className={styles.salaryBlock}>
               <div className={styles.salaryTitle}>Средний</div>
-              <div className={styles.salaryValue}>{page.hh?.middleSalary}</div>
+              <div className={styles.salaryValue}>{page.hh?.middleSalary && priceRu(page.hh?.middleSalary)}</div>
               <div className={styles.salaryRating}>
                 <RoundStarIcon color="#FC836D" />
                 <RoundStarIcon color="#FC836D" />
@@ -60,7 +61,7 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 
             <div className={styles.salaryBlock}>
               <div className={styles.salaryTitle}>Профессионал</div>
-              <div className={styles.salaryValue}>{page.hh?.seniorSalary}</div>
+              <div className={styles.salaryValue}>{page.hh?.seniorSalary && priceRu(page.hh?.seniorSalary)}</div>
               <div className={styles.salaryRating}>
                 <RoundStarIcon color="#FC836D" />
                 <RoundStarIcon color="#FC836D" />
